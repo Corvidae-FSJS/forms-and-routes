851d3689c20133c4a28df81377914a3877b57539
Dylan Corvidae <dylancorvidae@gmail.com>
2019-11-04 17:31:14 -0800

    showing random character

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import getCharacter from '../services/api-call';
 
 const Character = ({ character }) => {
   return (
     <div>
-      {character}
+      <img src={character.photoUrl} />
+      <h2>{character.name}</h2>
     </div>
   );
 };
